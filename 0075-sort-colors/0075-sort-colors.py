5db0f8e0e84b64ddfee16bb749ade83378b27c9d
Bharath M P Hegde <151370769+BharathMPhegade@users.noreply.github.com>
2026-07-06 13:49:38 +0530

    Time: 2 ms (18.86%), Space: 12.3 MB (54.3%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,13 +1,18 @@
 class Solution(object):
     def sortColors(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        for i in range(n):
-            for j in range(0,n-i-1):
-                if nums[j] >nums[j+1]:
-                    nums[j],nums[j+1] = nums[j+1],nums[j]
-        
-        return nums
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
