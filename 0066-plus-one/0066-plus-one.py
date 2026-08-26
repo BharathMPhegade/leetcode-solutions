8f682874ffcfc4ce6029b7eba19533d3bfbd247b
Bharath M P Hegde <151370769+BharathMPhegade@users.noreply.github.com>
2026-08-26 18:26:45 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,18 +1,16 @@
 class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        num = 0
-        for i in digits:
-            num =(num*10)+i
-        
-        num = num+1
-        s = str(num)
-        n = len(digits) - 1
-        res = []
-        for i in s:
-            res.append(int(i))
-        return res
 
+    def plusOne(self, digits):
+
+        for i in range(len(digits) - 1, -1, -1):
+
+            # If the digit is less than 9
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            # If the digit is 9, make it 0
+            digits[i] = 0
+
+        # If all digits were 9, for example [9,9] → [1,0,0]
+        return [1] + digits
